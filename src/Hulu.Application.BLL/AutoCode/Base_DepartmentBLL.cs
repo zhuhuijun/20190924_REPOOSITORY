3316b1a4238953c17f78ca4f62865832bbb50362
zhuhuijun <zhuhuijun@beisen.com>
2019-09-24 18:13:43 +0800

    bll

--- src/Hulu.Application.BLL/AutoCode/Base_DepartmentBLL.cs
+++ src/Hulu.Application.BLL/AutoCode/Base_DepartmentBLL.cs
@@ -3,7 +3,7 @@
 *│　描    述：无                                                    
 *│　作    者：zhuhj                                              
 *│　版    本：1.0   模板代码自动生成                                              
-*│　创建时间：2019-09-24 18:04:32                            
+*│　创建时间：2019-09-24 18:12:41                            
 *└──────────────────────────────────────────────────────────────┘
 *┌──────────────────────────────────────────────────────────────┐
 *│　命名空间:Hulu.Application.BLL                              
@@ -13,14 +13,14 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-using Hulu.Data.Repository;
 using Hulu.Application.IService;
+using Hulu.Application.Service;
 using Hulu.Util.Model;
 
-namespace Hulu.Application.Service
+namespace Hulu.Application.BLL
 {	
 	/// <summary>
-	/// IBase_DepartmentService
+	/// Base_DepartmentBLL
 	/// </summary>	
 	public partial class Base_DepartmentBLL
     {
