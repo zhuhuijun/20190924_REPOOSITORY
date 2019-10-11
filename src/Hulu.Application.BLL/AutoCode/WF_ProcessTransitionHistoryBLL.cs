c504b066c8c690d213164438dd78d99d4ef651e8
zhuhuijun <zhuhuijun@beisen.com>
2019-10-11 14:14:36 +0800

    fsf

--- src/Hulu.Application.BLL/AutoCode/WF_ProcessTransitionHistoryBLL.cs
+++ src/Hulu.Application.BLL/AutoCode/WF_ProcessTransitionHistoryBLL.cs
@@ -3,7 +3,7 @@
 *│　描    述：无                                                    
 *│　作    者：zhuhj                                              
 *│　版    本：1.0   模板代码自动生成                                              
-*│　创建时间：2019-09-24 18:12:41                            
+*│　创建时间：2019-09-24 20:12:16                            
 *└──────────────────────────────────────────────────────────────┘
 *┌──────────────────────────────────────────────────────────────┐
 *│　命名空间:Hulu.Application.BLL                              
@@ -33,6 +33,60 @@ namespace Hulu.Application.BLL
         {
             return service.GetList_Auto();
         }
+        /// <summary>
+        /// GetPageList
+        /// </summary>
+        /// <param name="pagination">分页</param>
+        /// <param name="queryJson">查询参数</param>
+        /// <returns></returns>
+        public IEnumerable<WF_ProcessTransitionHistoryEntity> GetPageList(Pagination pagination, string queryJson)
+        {
+            return service.GetPageList_Auto(pagination, queryJson);
+        }
+        /// <summary>
+        /// GetEntity
+        /// </summary>
+        /// <param name="keyValue">主键值</param>
+        /// <returns></returns>
+        public WF_ProcessTransitionHistoryEntity GetEntity(string keyValue)
+        {
+            return service.GetEntity_Auto(keyValue);
+        }
+
+        #region 提交数据
+        /// <summary>
+        /// 删除职位
+        /// </summary>
+        /// <param name="keyValue">主键</param>
+        public void RemoveForm(string keyValue)
+        {
+            try
+            {
+                service.RemoveForm_Auto(keyValue);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        /// <summary>
+        /// 保存SaveForm
+        /// </summary>
+        /// <param name="keyValue">主键值</param>
+        /// <param name="oneEntity">oneEntity</param>
+        /// <returns></returns>
+        public void SaveForm(string keyValue, WF_ProcessTransitionHistoryEntity oneEntity)
+        {
+            try
+            {
+                service.SaveForm_Auto(keyValue, oneEntity);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        #endregion
     }
 }
 
